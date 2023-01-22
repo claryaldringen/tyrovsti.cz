3534e96269af1429a4ffb3a04c7333e85e8be62e
Martin Zadrazil <martin.zadrazil@ibm.com>
2023-01-22 07:53:04 +0100

    Žižkovo tažení do Uher

---
+++ components/Articles/ZizkovoTazeniDoUher.js
@@ -0,0 +1,137 @@
+import { Col, Row } from 'reactstrap'
+import React from 'react'
+
+export const ZizkovoTazeniDoUher = () => (
+  <>
+    <Row id="jana-hajka-z-hodetina-rad-vojensky">
+      <Col>
+        <h4>Žižkovo (fiktivní) vojenské tažení do Uher l. 1423</h4>
+        <p>...</p>
+        <p>....</p>
+        <p>&nbsp;</p>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={6}>
+        <p>
+          Potom jednoho času táhl jest Žižka do Moravy a podmaňoval sobě lidi; i
+          přitáhl také k Kramicu, a tu jest některý čas ležal. A bratří naň
+          domlúvali, aby někam taženie učinil; a on zpraviv čtyři řady vozuov, a
+          děl což mohl najviece, i táhl jest přes hory do Uher. A tu sú jemu
+          Uhři taženie nebránili, a nikdiež jemu nepřekáželi, chtiec zdaby se
+          ubezpečil i z řádu některak povystúpil, zdaby jej tak kde poraziti
+          mohli. Neb sú se naň velmi silně jezdecky sebrali, a sebravše se naň
+          sú táhli, i množstvie děl měli. A on porozuměv tomu, že dobře nenie,
+          obrátiv sobě k Moravské zemi zase vozy, i takto je zpravil najprvé
+          sobě lidi u voziech zpravil, jiezdné i pěší, a táhl jest s zavřenými
+          vozy. A takto sobě krajní vozy zpra- vil: proti každým vozuom se dvěma
+          pavézama, a za ty pavézy po dvú neb po třech střelcích; a to z té
+          příčiny udělal, jest-li žeby mu na tažení chtěli do vozuov skočiti,
+          aby toho ti bránili. Neb ten den těžké mu taženie před nimi přišlo;
+          neb kdež se najméně pozastavil, velmi mu do vojska bili. I nemohl jim
+          nic odolati, ale táhl jest až do noci; a to z té příčiny, aby ho
+          nemohli tak v noci shlednúti, kde se položí, a do vojska mu bíti; a v
+          svém vojště nedal žádných ohňuov dělati. A oni pojezdiec okolo něho,
+          nemajíc kde koňuov vázati, musili do vsí jeti a to podál, aby před ním
+          také bezpečni byli. Tu on poležav, což mu se zdálo, táhl jest
+          nazajtřie k jednomu jezeru, kdežto jezero mezi jedniem vrchem. A takto
+          se u něho položil: jednú stranú k jezeru a druhú stranú pod vršek; a
+          to z té příčiny, chtěli-li by do vojska střieleti, aby jej
+          přestřelovali. A sám takto udělal: pojav pícní vozy, vytočil je na ten
+          vršek, udělal z nich u přední brány baštu i u zadní druhú; i vkopal
+          je, a vkopav je vpravil do nich děla. I nedal jim nikdiež na těch
+          vrších ostati. A tak ten den bezpečně tu ležal i tu noc. Opět potom
+          třetí den od toho jezera táhl jest k jedné vodě, kteráž teče k
+          Narhedu, i položil se u té vody; i tu jest se velmi bál poraženie, aby
+          přepustíc mu něco lidí přes tu vodu, (do zadních Vozuov jemu
+          nevskočili. A protož) celú noc vkopával krajní vozy od jednoho břehu
+          až k druhému; a z druhé strany vody přepravil sobě pícní vozy, i též
+          je vkopával tak, aby na ně úprkem jiezdní nemohli. A všecky břehy
+          skopal, aby mohl všemi čtyřmi řady táhnúti přes tu vodu. Kdež oni
+          vpustivše jej do té vody s vozy, i pokusili mu se k zadním vozuom;
+          kdež jest on je od těch zákopuov odbil, kdež sú i pěšky i jezdecky k
+          němu šturmovali. Tu jich je mnoho zranil i pobil; a odbiv je, na onu
+          stranu přepravil se s vozy i s lidem svým. I děkoval pánu Bohu, že mu
+          pán Buoh ráčil pomoci přes tu vodu se přepraviti. I hned táhl jest
+          odtud mezi jedny lesy, kdež jsú bahna a luhové, i položil se takto
+          mezi zákutí; a to z té příčiny, aby k němu z děl biti nemohli. I tu
+          jest přes noc zuostal. Čtvrtý den táhl jest k jezeruom jedněm, a
+          neboli k stavuom, kdež jsú za Trnavú. A tu sobě zpravoval vozy,
+          vybieraje z placních řaduov, a to z té příčiny, kdež mu v krajních vo-
+          zích v řadích mnoho koní i vozuov polámali i pobili. A tehdáž ho
+          ponechali. A on položiv se u těch stavuov, zpraviv sobě ty všecky
+          věci, v pátý den táhl jest k jedné stráni. Tu sú naň opět přitáhli
+          jezdecky i s děly. A on jest táhl podlé té stráně, že měl jednú stranú
+          obranu, že jemu snáze bylo jedné strany brániti nežli obú. A oni
+          vidúc, že jemu nemohú nic učiniti, i táhli sú pryč od něho; a on
+          položil se na jednom vrchu. Kdež tak pravie lidé, že jest veliká psota
+          a bieda tu na nich byla, tak že za jeden košťál zelený byli by drahně
+          peněz dali; a to z té příčiny, že jim z vozuov nikam nedali, že sú mu
+          jiezdnými silni byli, a naň se dáti pěšky nesměli. Než když kolivěk je
+          k tomu připravili, že sú jiezdní doluov ssedali a pěšky k němu
+          šturmovali, neb se s ním bili, že je vždycky porazil, aneb od něho
+          velmi biti byli. Neb jináč se strojí jezdecky k bitvě a jináč pěšky,
+          že jim to jest neobyčejná věc; neb kdež se mají čemu učiti, zvyknú,
+          žeby musil k tomu býti neb nějaká chvíle. než tomu nějaký čas.
+        </p>
+        <p>
+          Šesté taženie Žižkovo, když se hnul od té stráně, kdež se jemu dostalo
+          táhnúti k horám, i kdež mu ten den na jeden řad vozuov přišlo k
+          tažení, že jináč táhnúti nemohl. Tehdá Uhři vidúce to, táhli naň velmi
+          silně, nadějíce se, že již jeho zpráva ani jeho šikové nic jemu
+          neprospějí, že ven z vozuov musejí. A on takto proti tomu strojil,
+          bráně se toho: položil se u samých lesov pod jedním vrškem, a to pro
+          tu příčinu, budú-li k němu z děl bíti, aby jej přestřelovali; a sám s
+          svými děly utekl se k tomu vršku. A potom kázal koně z vozuov
+          vypřahati, a na ně s sekyrami, s lopatami a s rejči vsedati, aby cesty
+          zohledovali, jest-li žeby jemu kde zarubali; a to z té příčiny, aby po
+          stranách na ně na vozy jiní udeřiti nemohli, cesty velmi dobře kázal
+          opraviti. A když měl z lesuov ven vytáhnúti, as za puoldruhých honuov,
+          kázal s jedné strany cesty staré novú cestu dělati, a s druhé strany
+          druhú; a to z té příčiny, aby předtáhnú-li jej nepřátelé, aby tú starú
+          cestú děla a pěší sobě obrátil; a to proto, jest-li žeby mu chtěli
+          toho pole brániti, kudyž jest z lesu měl vyjeti, aby je tudy vystřílel
+          a vybil; jakož se jest pak stalo tak. I takto s oné strany lesuov vozy
+          sobě zpravili: udělal sobě z těch čtyř řaduov, v kterýchž táhl, dva; a
+          pojav sobě pícovní vozy, i protrhl sobě vozy, i udělal sobě z nich
+          řady čtyřmi jakožto zed, aneb jakožto baštu od jednoho konce lesu až k
+          druhému. A kázal je húžvemi svírati v hromadu, aby mu jich roztrhnúti
+          aneb převraceli tak snadně nemohli. A to udělal proto, jest-li žeby
+          chtěli jemu do vozuov skočiti, aneb děla z zadu pobrati, aby oni tu se
+          bránili za těmi vozy jako za zdí; jakož se pak tak stalo. I takto jest
+          púštěl do lesu, najprvé děla, což mu se zdálo, i pustil něco pěších, a
+          potom padesát vozuov po pravé ruce. A když těch padesát vozuov vyšlo
+          jest, opět dal mezi ně něco pěších, což mu se zdálo; i pustil opět
+          druhých padesát vozuov z levé strany, a opět mezi ně dal něco pěších.
+          A tak vždycky dával, pokudž mu těch vozuov stačilo, po padesáti; a
+          mezi ně pěší dával, pokudž ta potřeba kázala. A to z té příčiny,
+          jest-li žeby mu chtěli na lesích vozataje zbíti, aneb vozy také
+          vybrati, aby ti pěší, kteří jdú mezi vozy, toho bránili. Tehdy kdy
+          poslední vozy púštěl na zad, tehdy obořili se Uhři, chtiec mu děla po-
+          brati. Tehdy oni s toho vršku utáhli za ty pícovní vozy, kdež jest
+          sobě z nich řady zdělal; tu se v těch vozích zavřeli, a pěší se z nich
+          bránili, tak až oni s děly do lesa ujeli, i pěší potom za nimi táhli.
+          A tak se Uhruov nic nebáli; než sú se oni do těch vozuov dobyli, až
+          oni něco i cesty zarubali, aby za nimi jezdecky táhnúti nemohli. To
+          vidúce Uhři, že jemu žádné škody udělati nemohli, i někteří táhli
+          předeň, chtiec mu z lesuov brániti; a někteří táhli domuov, praviec
+          tak, že nenie člověk, než že je čert, že mu ty rozumy dává, neb že se
+          nám ničímž užiti nedá. I takto se jest z lesa vypravoval ven, kdež sú
+          byla nějaká pole mezi nějakými stráněmi: když přitáhl k těm cestám,
+          kdež sobě nové zdělal, najprvé těm padesáti vozuom kázal hnúti tú novú
+          cestu, kterú sobě novú udě- lal po pravé ruce; i když sú na pole vy-
+          táhli, podlé samé stráně, a to z té příčiny, že od pole měli pěší
+          vozy, a za sebú stráně, a tak tudy se jiezdných nebáli. A z druhého
+          řadu po levé straně též kázal hnúti podlé druhé stráně; a tú starú
+          cestú vypustil děla ven z lesa, i byl opanoval sobě to pole. A tak
+          pomalu vždy vozy těmi ven vytahoval; neb sobě byl též také vozy
+          zpravil všecky v jeden řad, že je zprovíjel jako věnec. A to proto
+          udělal, aby oba řady sobě pojednú dělal, aby je sobě z toho pole
+          vytiskl mocnú rukú. Neb tak jemu těch vozuov vždy přibývalo ven, až
+          jimi všemi vytáhl ven. A tak jemu pán Buoh pomohl z Uher vytáhnúti.
+          Ale jakž jest Žižka počal bojovati, najtěžší jemu přišlo.
+        </p>
+      </Col>
+    </Row>
+  </>
+)
