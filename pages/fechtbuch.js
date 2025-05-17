f243711fe064cbea1e24b06d069da57120fa553b
Martin Zadražil <zadram1@gmail.com>
2025-05-17 15:48:41 +0200

    feat: Mayers fechtbuch

---
+++ pages/fechtbuch.js
@@ -0,0 +1,188 @@
+import React from 'react'
+import { Col, Container, Row } from 'reactstrap'
+import { HeadExtended } from '../components/HeadExtended'
+import { LANG_EN } from '../shared/constants'
+import { ImageWithTitle } from '../components/ImageWithTitle'
+
+const Page = () => {
+  return (
+    <>
+      <HeadExtended lang={LANG_EN} title="Saddle" />
+      <Container>
+        <Row>
+          <Col className="text">
+            <Row>
+              <Col>
+                <h1>Fechtbuch Joachima Meyera (MS Bibl. 2465)</h1>
+                <p>
+                  <b>MS Bibl. 2465</b> je německý fechtbuch sepsaný Jocahimem
+                  Meyerem roku 1561.
+                </p>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h4>Pěší boj ve zbroji (folio 101r - 112r)</h4>
+                <ul>
+                  <li>
+                    Vstupuj tak, aby tvá levá noha byla neustále vpředu. Nebuď
+                    příliš zbrklý a sleduj, zda dosáhneš na jeho přední nohu.
+                    Ukaž, jako bys chtěl zaútočit na jeho obličej, ihned pusť
+                    rukojeť pravou rukou, přejdi levou na pomoc a udeř ho
+                    hlavicí meče do hlavy nebo do jeho levé paže. Pokud tvůj
+                    úder zablokuje mezi oběma rukama, nech ho projet kolem hlavy
+                    a udeř ho zprava do kolenního kloubu nebo kotníku.
+                  </li>
+                  <li>
+                    Dále jdi do horního postavení (Oberhut) jako předtím a dávej
+                    pozor, zda ti chce přiložit (ansetzen) nebo zaútočit. Nastav
+                    tedy levou ruku vpřed (absetzen). Pokud tě pak udeří shora,
+                    odraz úder mezi oběma rukama a okamžitě bodni zvenčí přes
+                    jeho paži a nasaď (ansetzen) hrot do hrdla. Jakmile zjistíš,
+                    že se hrot zasekl (ve zbroji), polož obě ruce pod své pravé
+                    rameno a nenech ho uniknout. Naopak, tlač vpřed.
+                  </li>
+                  <li>
+                    Dále vejdi do horního postavení a bodni do jeho hledí. Pokud
+                    se brání, nasaď zbraň do jeho levého ramene a tlač vpřed.
+                    Dávej pozor, jakmile se bude chtít vyprostit, že se na něj
+                    vrhneš celou vahou pomocí zápasu, podhozu nebo bodnutí do
+                    přední části hledí.
+                    <br />
+                    Pamatuj: pokud máš někoho zablokovaného, nenech ho uniknout
+                    – pak nebude moci dále bojovat.
+                    <br />V druhém postavení drž meč rukojetí vedle pravého
+                    boku. Uchop čepel levou rukou uprostřed, tak aby hrot
+                    směřoval na protivníka, a levá noha byla stále vpředu.
+                    Přibližuj se. Pokud bodá, odraz to od ruky. Pokud seká,
+                    přijmi úder na čepel mezi obě ruce. Inndes sleduj, kdy
+                    udeří, a vykroč pravou nohou, odraz tak, jak ses naučil, a
+                    udeř hlavicí do podkolenní jamky. Bude chtít stáhnout nohu,
+                    takže mu nasaď zbraň do jeho pravé podpaží nebo kamkoli to
+                    půjde a odtlač ho od sebe, nebo udeř hlavicí do jeho krku,
+                    vykroč pravou nohou za jeho nohu a poraz ho.
+                  </li>
+                  <li>
+                    Dále odraž jeho úder z postavení a vyraž vzhůru s rukojetí,
+                    a bodni ho do krku nebo do otevřených míst u kloubů. Stejně
+                    tak odraž jeho bodnutí, a proveď totéž – a jakmile ho máš
+                    zablokovaného, udeř hlavicí pod paži, abys si zajistil
+                    výhodu.
+                    <br />
+                    Přiložení (ansetzen) jsou čtyři: jedno do obličeje, jedno do
+                    krku. Stejně tak pod levou paži nebo pod pravé rameno – a
+                    všimni si, že když mu zbraň přiložíš zleva, přistup levou
+                    nohou vpřed a polož ji pod jeho pravé rameno, pak přistup
+                    pravou nohou. Dále, když máš zbraň přiloženou na soupeře,
+                    vždy ho veď směrem vzhůru. Je-li menší než ty, spusť rukojeť
+                    pod sebe k bokům a tlač směrem vzhůru. Pokud se vychýlí do
+                    stran, bodni mu do helmy. Tam je nejzranitelnější.
+                    <br />
+                    Jakmile mu přiložíš (ansetzen) zbraň, ujisti se, že jsi
+                    zcela chráněn zbrojí. Když přiložíš správně a tlačíš
+                    správným směrem, protivník ti nedokáže účinně čelit.
+                  </li>
+                  <li>
+                    Jestliže ti přiložil (ansetzen) svou zbraň aby tě mohl
+                    znehybnit , vraž hrot vzhůru přes jeho pravou paži dovnitř
+                    pod jeho meč a tlač jílec dolů směrem k zemi. Jakmile se
+                    uvolní, prostrč meč vpřed pod jeho pravou paži, pravou nohu
+                    dej za jeho levou a levou rukou chyť jeho přilbu a shoď ho.
+                    Také ho bodni do rukavice na přední ruce, veď ho dokola,
+                    nebo ho bodni pod jeho levou paži vzhůru přes jeho meč. Tlač
+                    jílec vzhůru a znehybni (ansetzen) ho (soupeře).
+                  </li>
+                  <li>
+                    Také si při zápase všímej, z které strany ti přiložil
+                    (ansetzen). Chyť jeho čepel levou rukou, odsuň ji od sebe,
+                    pak ji rychle přitáhni zpět k sobě a chyť jeho pravou ruku
+                    pravou rukou, přitáhni si ho k sobě, obejmi jeho tělo a
+                    zvedni ho vzhůru. On se bude zvedat. Podkopni mu nohu a shoď
+                    ho od sebe.
+                  </li>
+                  <li>
+                    Také, pokud na tebe bodá, bodni s ním zároveň na jeho levou
+                    stranu a při tom zachyť jeho čepel svou a protlač svou
+                    hlavici zespodu mezi obě jeho paže a udeř vzhůru, aby musel
+                    pustit.
+                  </li>
+                  <li>
+                    Také dávej pozor, kterou rukou ho můžeš zachytit, a strhni
+                    ho k sobě, druhou rukou uchop jeho loket (nebo lokty),
+                    přenes na něj váhu a postupuj krok za krokem, ale ne příliš
+                    zbrkle, aby ses dostal do stabilního postoje.
+                  </li>
+                  <li>
+                    Také, pokud jsi někoho srazil k zemi a on padá, padni s ním
+                    na jeho pravou stranu a polož své pravé koleno mezi jeho
+                    nohy a levou rukou ho obejmi kolem krku. Pokud ho máš
+                    bezpečně pod kontrolou, vytáhni dýku. Pokud se ti po ní
+                    sápe, bodni ho do ruky a stáhni ji zpět, dokud lépe
+                    nezpozoruješ svou výhodu.
+                  </li>
+                  <li>
+                    Také, další způsob: vezmi jeho pravou ruku mezi své nohy a
+                    lehni si na něj napříč/diagonálně, přičemž drž nohy téměř u
+                    sebe a natáhni se. Jeho levou ruku uchop svou levou rukou a
+                    pevně ji drž a pokračuj v boji s bojovým mečem, jak bude
+                    popsáno dále.
+                  </li>
+                </ul>
+                <h4>Jak používat tři výše zmíněné zbraně</h4>
+                <p>
+                  Poznámka: při prvním přístupu jdi do horního střehu (Oberhut)
+                  a naznač pohybem, jako bys chtěl zaútočit na jeho obličej.
+                  Neútoč však, místo toho udeř nebo sekej shora dolů na jeho
+                  kolenní přezky. Ať už ho zasáhneš, nebo ne, rychle zvedni ruce
+                  vzhůru a udeř ho kladivem (záštitou) do hlavy zvenčí přes jeho
+                  pravou ruku, nebo ho zasáhni do nejbližšího kloubu, na který
+                  dosáhneš.
+                </p>
+                <p>
+                  Rychle otoč zbraní kolem hlavy a nech pravou ruku projet před
+                  levou, pak uchop čepel pod levou rukou a udeř středním sekem
+                  (Mittelschlag) zprava na jeho přilbu – buď kladivem, jilcem
+                  nebo hruškou. Pokud chytí tvůj jílec, znovu použij pravou ruku
+                  a ustup zpět, abys mohl obnovit svou pozici, a pečlivě dávej
+                  pozor, aby tě nezasáhl. Také si nenech přiložit jeho zbraň
+                  (ansetzen) – spíš ustupuj v kruhu, jak dlouho to vydržíš.
+                </p>
+                <h4>Druhý způsob</h4>
+                <p>
+                  Poznámka: přistup k němu v horním střehu (Oberhut). Uchop
+                  jílec blízko hlavice a levou rukou uchop čepel u záštity a
+                  silně ho bodni do hledí. Poté stáhni zpět k sobě a udeř silou
+                  zespodu na jeho genitálie, aby se k tobě nemohl přiblížit.
+                  Opět stáhni svou zbraň zpět a uchop levou rukou první část
+                  čepele.
+                  <br />
+                  Pokud na tebe zaútočí, ustup dozadu, abys ho přinutil vykročit
+                  vpřed a případně tě zasáhnout. Nedovol mu tě zasáhnout –
+                  raději ustup. Pokud zaútočí, dávej pozor, mohl by tě zmást
+                  klamným výpadem, takže pusť pravou ruku a přispěj levou a udeř
+                  ho do přilby nebo do nejbližšího kloubu. Pokud však
+                  nezaútočil, udeř ho do kolena nebo kotníku. Pokud úder odrazí,
+                  zvedni paže a udeř ho do horního kloubu, a opět uchop oběma
+                  rukama. <br />
+                  Pokud se ti však nepodaří proniknout úderem, odraž horní útok
+                  mezi oběma rukama na čepeli. Současně hoď hlavici přes levé
+                  rameno a levou rukou zvedni, přičemž pravou rukou pomoz levé.
+                  Tím zasáhneš dříve, než se stihne zotavit.
+                </p>
+                <ImageWithTitle src="mayer1.jpg" width={890} height={628} />
+                <p>
+                  Poznámka: když k tobě přistupuje v horním střehu (Oberhut),
+                  pokud bodá nebo seče, tak odraž útok levou rukou, a při tom
+                  zachyť jeho čepel svojí a veď jílec zespodu skrz, přistup
+                  pravou nohou za jeho levou a sraž ho na zem.
+                </p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Page
