94fe86cb52da739f8e7b3a762f18a2dd35d414a9
Martin Zadražil <zadram1@gmail.com>
2025-07-19 15:30:48 +0200

    feat: Waldshuterkrieg

---
+++ pages/prehled-vojenskych-akci/index.js
@@ -0,0 +1,29 @@
+import { Col, Container, Row } from 'reactstrap'
+import React from 'react'
+import { HeadExtended } from '../../components/HeadExtended'
+import { Sources } from '../../components/Quote/Sources'
+import { Payment } from '../../components/Payment'
+import { Waldshuterkrieg } from '../../components/Articles/cz/akce/Waldshuterkrieg'
+
+const Page = () => {
+  return (
+    <>
+      <HeadExtended
+        title="Přehled vojenských akcí 1434 - 1525"
+        image="orsha.jpg"
+      />
+      <Container>
+        <Row>
+          <Col className="text">
+            <h1>Přehled vojenských akcí 1434 - 1525</h1>
+            <Waldshuterkrieg />
+            <Sources />
+            <Payment />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Page
